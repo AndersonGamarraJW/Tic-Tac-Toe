cbbc8d9f7244f7ac77d4fd92d9f3d8a03f11ba84
AndersonGamarraJW <andersong.v3105@gmail.com>
2023-01-31 18:44:55 -0500

    feat: Crea la logia del juego para validar ganador

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const tablero = (function(){
     let xCounter = 0;
     let oCounter = 0;
-    const matriz = [['','','x'],['','','x'],['','','x']];
+    const matriz = [
+        ['x','','x'],
+        ['','x','x'],
+        ['','','']
+    ];
     const setSimbol = (position,simbol) => {
         matriz[position[0]][position[1]] = simbol;
     }
@@ -9,7 +13,6 @@ const tablero = (function(){
     const _verifyDown = (tamañoTabla,simbol,column,row) =>{
         const max = tamañoTabla - row - 1;
         for(let i = 1 ;i <= max;i++){
-            console.log(row+i);
             if(matriz[row+i][column] != simbol) return false;
         }
         return true;
@@ -21,14 +24,59 @@ const tablero = (function(){
         }
         return true;
     }
+    const _verifyRight = (tamañoTabla,simbol,column,row) => {
+        const max = tamañoTabla - column - 1;
+        for (let i = 1;i <= max ;i++){
+            if(matriz[row][column+i] != simbol) return false;
+        }
+        return true;
+    }
+    const _verifyLeft = (simbol,column,row) =>{
+        const max = column;
+        for (let i = 1;i<=max;i++){
+            if(matriz[row][column-i] != simbol) return false;
+        }
+        return true;
+    }
+    const _rightDiagonal = (tamañoTabla,simbol,column,row) =>{
+        const max = tamañoTabla -column-1;
+        for (let i = 1; i <= max ;i++){
+            if(matriz[row-i][column+i] != simbol) return false
+        }
+        return true;
+    }
+    const _leftDiagonal = (simbol,column,row) => {
+        const max = column;
+        for (let i = 1;i<=max;i++){
+            if(matriz[row+i][column-i] != simbol) return false;
+        }
+        return true;
+    }
     const _completeColumn = (lastPosition,simbol) => {
-        if(_verifyUp(simbol,lastPosition[1],lastPosition[0])) console.log('Ganaste');
+        const [row,column] = lastPosition;
+        if(_verifyDown(3,simbol,column,row) && _verifyUp(simbol,column,row))
+            return true;
+        return false;
     }
-    
+    const _completeRow = (lastPosition,simbol) => {
+        const [row,column] = lastPosition;
+        if(_verifyRight(3,simbol,column,row) && _verifyLeft(simbol,column,row))
+            return true;
+        return false;
+    }
+    const _completeDiagonal = (lastPosition,simbol) => {
+        const [row,column] = lastPosition;
+        if(_rightDiagonal(3,simbol,column,row) && _leftDiagonal(simbol,column,row))
+            return true;
+        return false;
+    }
     const completeRowColumn = (lastPosition,simbol) => {
+        if(_completeColumn(lastPosition,simbol) || _completeRow(lastPosition,simbol) || _completeDiagonal(lastPosition,simbol))
+            console.log('Ganaste');
     }
     
-    return {_completeColumn}
+    
+    return {completeRowColumn,setSimbol}
 })();
 
 function playerFabric(name,simbol){
@@ -41,6 +89,6 @@ function playerFabric(name,simbol){
     return {getName,setName,getSimbol,setSimbol}
 }
 
-tablero._completeColumn([2,2],'x');
+tablero.completeRowColumn([2,0],'x');
 
 
