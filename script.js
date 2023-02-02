321d04739c5757207c8c28f70973c517ee650511
AndersonGamarraJW <andersong.v3105@gmail.com>
2023-02-01 19:45:23 -0500

    feat: Crea las funciones de cada celda del juego

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const tablero = (function(){
     let xCounter = 0;
     let oCounter = 0;
     const matriz = [
-        ['x','','x'],
-        ['','x','x'],
+        ['','',''],
+        ['','',''],
         ['','','']
     ];
     const setSimbol = (position,simbol) => {
@@ -41,7 +41,7 @@ const tablero = (function(){
     const _rightDiagonal = (tamañoTabla,simbol,column,row) =>{
         const max = tamañoTabla -column-1;
         for (let i = 1; i <= max ;i++){
-            if(matriz[row-i][column+i] != simbol) return false
+            if(row-i <0 || matriz[row-i][column+i] != simbol) return false
         }
         return true;
     }
@@ -74,9 +74,11 @@ const tablero = (function(){
         if(_completeColumn(lastPosition,simbol) || _completeRow(lastPosition,simbol) || _completeDiagonal(lastPosition,simbol))
             console.log('Ganaste');
     }
+    const printMatriz = () => {
+        for (let i=0;i<matriz.length;i++) console.log(matriz[i])
+    }
     
-    
-    return {completeRowColumn,setSimbol}
+    return {completeRowColumn,setSimbol,printMatriz}
 })();
 
 function playerFabric(name,simbol){
@@ -89,6 +91,40 @@ function playerFabric(name,simbol){
     return {getName,setName,getSimbol,setSimbol}
 }
 
-tablero.completeRowColumn([2,0],'x');
+const boxes = document.querySelectorAll('.box');
 
+function setSimbol(){
+    let position = getGridElementsPosition(getNodeIndex(this));
+    tablero.setSimbol(position,'x');
+    tablero.printMatriz();
+    tablero.completeRowColumn(position,'x');
 
+}
+boxes.forEach(box=>{
+    box.addEventListener('click',setSimbol);
+});
+
+function getGridElementsPosition(index){
+    //Toma la contenedora grid, la que almacena todos los grid-elements
+    const gridEl = document.getElementById('template-container'); 
+    
+    let offset = Number(window.getComputedStyle(gridEl.children[0]).gridColumnStart) -1;
+    
+    if(isNaN(offset)) offset = 0;
+    
+    //Contador de columnas
+    const colCount = window.getComputedStyle(gridEl).gridTemplateColumns.split(' ').length;
+    
+    const rowPosition = Math.floor((index + offset) / colCount);
+    const colPosition = (index + offset) % colCount;
+    
+    return [rowPosition,colPosition]
+}
+
+//Obtener el index del nodo 
+function getNodeIndex(elm){
+    const c = elm.parentNode.children;
+    for(let i =0;i<c.length;i++){
+        if(c[i] === elm) return i
+    }
+}
